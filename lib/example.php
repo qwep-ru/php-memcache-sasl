7b6a5c5c79b84373f7c182cd2d682ea656e86419
iamdimas <dmi.vyatkin@gmail.com>
2016-10-28 12:58:08 +0300

    add gitignore

--- lib/example.php
+++ lib/example.php
@@ -3,8 +3,7 @@
 include('MemcacheSASL.php');
 
 $m = new MemcacheSASL;
-$m->addServer('mc7.ec2.northscale.net', '11211');
-$m->setSaslAuthData('username', 'password');
+$m->addServer('185.96.86.124', '11211');
+$m->setSaslAuthData('memcache_user', 'MemcachedHardPasswordForaccessItNow');
 var_dump($m->add('test', '123'));
 $m->delete('test');
-
